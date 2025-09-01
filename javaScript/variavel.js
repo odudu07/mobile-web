807cde5d2b8f04d4d273224ec3ceea2583e235c7
giovannirp <giovanni_ge@hotmail.com>
2025-09-01 15:52:31 -0300

    feat: aprendendo variáveis

---
+++ javaScript/variavel.js
@@ -0,0 +1,10 @@
+let mensagem = "Olá, mundo";
+
+const nomeCompleto = "Giovanni Ribeiro";
+
+let idade = 37;
+
+// Resultado
+console.log(mensagem);
+console.log(nomeCompleto);
+console.log("Meu nome é: " + nomeCompleto + " e minha idade é: " + idade + " Anos");
