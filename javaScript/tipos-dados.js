3c5cddaa045ab6bbcc4074d8219b4c4c77027312
giovannirp <giovanni_ge@hotmail.com>
2025-09-01 16:04:45 -0300

    feat: adicionando tipos de dados

---
+++ javaScript/tipos-dados.js
@@ -0,0 +1,19 @@
+// String
+let nome = "Giovanni";
+
+// Inteiro
+let idade = 37;
+
+// Booleano
+let casado = true;
+
+//  Números flutuantes com casas decimais
+let valorProduto = 5.35;
+
+// Resultados
+console.log("Meu nome é " + nome);
+console.log("Minha idade é: ", idade);
+console.log("Casado? ", casado);
+console.log("R$", valorProduto);
+console.log(typeof idade);
+console.log(typeof nome);
