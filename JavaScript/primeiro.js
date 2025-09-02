e29136af4587d75ddb4f1d94a604f5f986419b15
odudu07 <eduardo.arocha2024@gmail.com>
2025-09-02 13:59:27 -0300

    java

---
+++ JavaScript/primeiro.js
@@ -0,0 +1,5 @@
+let nome = "Eduardo";
+
+console.log(nome);
+console.log("Meu time do coração é o São Paulo");
+console.log("Eu tenho 18 anos");
---
+++ JavaScript/variavel.js
@@ -0,0 +1,10 @@
+let mensagem = "Olá, mundo!";
+
+const nomeCompleto = "Eduardo Alberto da Rocha";
+
+let idade = 34;
+
+// resultado
+console.log(mensagem);
+console.log(nomeCompleto);
+console.log("Meu nome é: " + nomeCompleto + ", minha idade é: " + idade + " anos");
