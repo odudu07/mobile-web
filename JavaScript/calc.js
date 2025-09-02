e976714d5380e85da53433cb5c647efd450ea064
odudu07 <eduardo.arocha2024@gmail.com>
2025-09-02 16:19:07 -0300

    javascript

--- JavaScript/calc.js
+++ JavaScript/calc.js
@@ -1,15 +1,14 @@
 // Duas variáveis
-let num1 = Number (prompt("Digite um número"));
-let num2 = Number (prompt("Digite outro número"));
+let num1 = 10;
+let num2 = 10;
 
-//opérações
+
+//operações
 let soma = num1 + num2;
-let sub = num1 - num2;
-let mult = num1 * num2;
-let div = num1 / num2; 
-
-// imprimir as respostas
-console.log("Soma", soma);
-console.log("subtração" , sub);
-console.log("multiplicação" , mult);
-console.log("divisão" , div);
+let subtracao = num1 - num2;
+   
+//funções
+function somar() {
+     console.log(soma);
+}
+  
