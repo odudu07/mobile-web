0b152fd8b0f6bcb8fc4587dfba255a31c197ade7
giovannirp <giovanni_ge@hotmail.com>
2025-09-02 14:25:49 -0300

    feat: adicionando funções nativas

---
+++ javaScript/exer1.js
@@ -0,0 +1,14 @@
+let nome = "Giovanni Ribeiro";
+let letras = nome[0] + nome[9];
+
+let linguagem = "JavaScript";
+let imprimirJs = linguagem[0] + linguagem[4];
+
+/*
+    JavaScript
+    js
+*/
+
+console.log(letras);
+console.log(imprimirJs);
+
