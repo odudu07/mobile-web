3f15202e2dd3fd84bc07c3528bd2ee783eb8b3a0
giovannirp <giovanni_ge@hotmail.com>
2025-09-02 16:18:30 -0300

    feat: adicionando codigo em js

--- javaScript/calc.js
+++ javaScript/calc.js
@@ -1,15 +1,11 @@
 // Duas variáveis
-let num1 = Number(prompt("Digite um número"));
-let num2 = Number(prompt("Digite outro número"));
+let num1 = 10;
+let num2 = 10;
 
 // Operações
 let soma = num1 + num2;
-let sub = num1 - num2;
-let mult = num1 * num2;
-let div = num1 / num2;
 
-// Imprimir as respostas
-console.log("Soma: ", soma);
-console.log("Subtração: ", sub);
-console.log("Multiplicação: ", mult);
-console.log("Divisão: ", div);
+// funções
+function somar() {
+    console.log(soma);
+}
