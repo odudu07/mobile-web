c40ab7a213887ccaf0b1b9e83e56162bbc55efc0
giovannirp <giovanni_ge@hotmail.com>
2025-09-02 15:05:36 -0300

    adicionando script

---
+++ javaScript/calc.js
@@ -0,0 +1,17 @@
+alert("teste");
+
+// Duas variáveis
+let num1 = 20;
+let num2 = 10;
+
+// Operações
+let soma = num1 + num2;
+let sub = num1 - num2;
+let mult = num1 * num2;
+let div = num1 / num2;
+
+// Imprimir as respostas
+console.log("Soma: ", soma);
+console.log("Subtração: ", sub);
+console.log("Multiplicação: ", mult);
+console.log("Divisão: ", div);
---
+++ javaScript/exer2.js
@@ -0,0 +1,7 @@
+/*
+    - Exiba no console
+      a quantidade de caracteres que a const possui
+*/
+const linguagem = "JavaScript";
+
+console.log(linguagem.length);
