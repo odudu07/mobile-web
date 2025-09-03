ade69c5c1f27975bf5c10c8a860b15e99fad23de
giovannirp <giovanni_ge@hotmail.com>
2025-09-03 14:53:45 -0300

    feat: adicionando codigo

--- javaScript/calc.js
+++ javaScript/calc.js
@@ -4,8 +4,17 @@ let num2 = 10;
 
 // Operações
 let soma = num1 + num2;
+let sub = num1 - num2;
+let mult = num1 * num2;
+let div = num1 / num2;
 
 // funções
 function somar() {
     console.log(soma);
 }
+
+function subFunction() {
+    console.log(sub);
+}
+
+
