2f26ba90207a7aac39ef46fd2257263db578240d
odudu07 <eduardo.arocha2024@gmail.com>
2025-09-03 16:13:22 -0300

    HAVA

--- JavaScript/calcNew.js
+++ JavaScript/calcNew.js
@@ -1,12 +1,20 @@
-// duas variaveis
-let num1 = 10;
-let num2 = 10;
-
 //funções
-function somar() {
-  console.log(num1, num2)
+function somar(num1, num2) {
+  console.log(num1 - num2)
 }
-  
 
+// sub 
+function sub(inden,num1,num2) [
+console.log(Inden, num1 - num2)
+]
+
+// mutl
+
+// div
+  
 // imprimir funções
-somar();
+somar(10, 10); // 10 + 10
+somar(10, 20); // 10 + 20
+somar(10, 15); 
+somar(3, 3);
+sub("sub", 15, 10); 
