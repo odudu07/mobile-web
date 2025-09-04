84553dd082988576c385e3db7a57ff1ed399e731
giovannirp <giovanni_ge@hotmail.com>
2025-09-04 16:22:39 -0300

    feat: add

--- javaScript/calculadora.js
+++ javaScript/calculadora.js
@@ -1,4 +1,12 @@
 // Variável
 const num1 = document.getElementById("numero1");
 const num2 = document.getElementById("numero2");
+const resultado = document.getElementById("resultado");
 
+// Função
+function soma(event) {
+   event.preventDefault(); 
+   resultado.innerHTML = Number(num1.value) + Number(num2.value);
+   num1.value = "";
+   num2.value = "";
+}
