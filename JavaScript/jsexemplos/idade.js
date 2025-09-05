fe56ab83fc1f132609184575af471042d5275e46
odudu07 <eduardo.arocha2024@gmail.com>
2025-09-05 14:52:21 -0300

    idade

---
+++ JavaScript/jsexemplos/idade.js
@@ -0,0 +1,12 @@
+/*
+    se a idade >= 18, pessoa pode ir para festa
+    senão , a pessoa não pode ir para festa
+*/
+
+let idade = 17;
+
+if (idade >= 18) {
+    console.log("A pessoa pode ir para festa");
+} else {
+    console.log("A pessoa não pode ir para festa");
+}
---
+++ JavaScript/jsexemplos/nota.js
@@ -0,0 +1,14 @@
+/*
+    se a nota for >= 6, o aluno está aprovado
+    senão , ele está reprovadp
+*/
+
+
+let nota = 7; 
+
+if (nota >= 6) {
+    console.log("O aluno está aprovado");
+} else {
+    console.log("O aluno está reprovado");
+}
+
