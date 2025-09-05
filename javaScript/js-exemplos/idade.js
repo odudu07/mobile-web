edcb19a549558b8efd79870b7e9284e863b15825
giovannirp <giovanni_ge@hotmail.com>
2025-09-05 14:50:44 -0300

    feat: estrutura de decisão

---
+++ javaScript/js-exemplos/idade.js
@@ -0,0 +1,11 @@
+/*
+    Se idade >= 18, pessoa pode ir para festa
+    senão, a pessoa não pode para festa
+*/
+let idade = 17;
+
+if (idade >= 18) {
+    console.log("Você pode ir para festa");
+} else {
+    console.log("Você não pode ir");
+}
---
+++ javaScript/js-exemplos/nota.js
@@ -0,0 +1,12 @@
+/*
+    Se a nota for >= 6, o aluno está aprovado
+    Senão ele está reprovado
+*/
+
+let nota = 4;
+
+if (nota >= 6) {
+    console.log("Você está aprovado");
+} else {
+    console.log("Você está reprovado")
+}
