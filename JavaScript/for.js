673dc76ee5c3479387d3b8ed5ac8f423e51031e0
odudu07 <eduardo.arocha2024@gmail.com>
2025-09-09 15:42:45 -0300

    For

---
+++ JavaScript/for.js
@@ -0,0 +1,7 @@
+for (let i = 0; i <=5; i++) {
+console.log("o valor de i, vale " + i);
+}
+
+for (let i = 1; i <= 10; i++) {
+console.log ("esta é a ${i} vez que esta frase é exibida no console");
+}
