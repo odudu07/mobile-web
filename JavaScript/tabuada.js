eaeaab86621888782827eac15d28c735436f0149
odudu07 <eduardo.arocha2024@gmail.com>
2025-09-09 16:10:58 -0300

    tabuada

---
+++ JavaScript/tabuada.js
@@ -0,0 +1,11 @@
+/*
+    Tabuada 7
+*/
+
+let multiplicador = 1;
+
+while (multiplicador <= 10) {
+    let resultado = 7 * multiplicador;
+    console.log(`7 x ${multiplicador} = ${resultado}`);
+    multiplicador++;
+}
