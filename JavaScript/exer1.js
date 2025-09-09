d7250bafc2d17b710b186ad6b1e8ba1b6b03d09e
odudu07 <eduardo.arocha2024@gmail.com>
2025-09-09 16:05:30 -0300

    while

---
+++ JavaScript/exer1.js
@@ -0,0 +1,5 @@
+const nomes = ["Giovanni", "Eduardo", "Doris", "Juliana"];
+
+for (let i = 0; i < nomes.length; i++) {
+    console.log(`- ${nomes[i]}`);
+}
---
+++ JavaScript/times.js
@@ -0,0 +1,5 @@
+const times = ("Bangu", "Paulista", "Vasco", "santos","Cotia");
+
+for (let i = 0; i < times.length; i++) {
+    console.log(`- ${times[i]}`);
+}
