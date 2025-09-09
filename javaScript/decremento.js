2dab8f32007a959e5bda103b74db1c86ad387849
giovannirp <giovanni_ge@hotmail.com>
2025-09-09 14:41:00 -0300

    feat: adicionando

---
+++ javaScript/decremento.js
@@ -0,0 +1,19 @@
+let valor = 7;
+
+valor = valor -1;
+
+console.log(valor);
+
+valor--;
+
+console.log(valor);
+
+/* Multiplicação */
+valor *= 2;
+
+console.log(valor);
+
+/* Divisão */
+valor /= 4;
+
+console.log(valor)
