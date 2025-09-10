e0fa8a1516481c7025bde8bf26f062af9a156ad3
giovannirp <giovanni_ge@hotmail.com>
2025-09-10 16:13:08 -0300

    feat: adicionando codigo novo

--- javaScript/nota.js
+++ javaScript/nota.js
@@ -3,6 +3,16 @@ const inputNota = document.getElementById("nota");
 const resultado = document.getElementById("result");
 
 // Função
-function resultadoNotaClick() {
-    alert("Resultado");
+function resultadoNotaClick(event) {
+    event.preventDefault()
+
+    // Lógica - Condição
+    if (inputNota.value >= 6) {
+        resultado.innerHTML = "Aprovado";
+    } else {
+        resultado.innerHTML = "Reprovado";
+    }
+
+    inputNota.value = "";
+
 }
