4e9379b36a09358a5cfd9a3307bf9b761ee07d5a
giovannirp <giovanni_ge@hotmail.com>
2025-09-11 14:41:09 -0300

    feat: adicionando logica de mensagem

--- javaScript/nota.js
+++ javaScript/nota.js
@@ -1,10 +1,19 @@
 // variáveis
 const inputNota = document.getElementById("nota");
 const resultado = document.getElementById("result");
+const message = document.getElementById("message");
 
 // Função
 function resultadoNotaClick(event) {
     event.preventDefault()
+
+    // Validação de formulário
+    if (inputNota.value == "") {
+        message.style.display = "block";
+        return false;
+    }
+
+    message.style.display = "none";
 
     // Lógica - Condição
     if (inputNota.value >= 6) {
