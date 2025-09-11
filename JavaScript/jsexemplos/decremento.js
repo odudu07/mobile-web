4c9160357134fd86d402549e46e4d773d8163c47
odudu07 <eduardo.arocha2024@gmail.com>
2025-09-11 13:55:09 -0300

    SEnai

---
+++ JavaScript/jsexemplos/decremento.js
@@ -0,0 +1,18 @@
+let valor = 7;
+
+valor = valor - 1;
+
+console.log(valor);
+
+valor--;
+
+console.log(valor);
+
+/* multiplicação*/
+valor *=2;
+console.log(valor);
+
+/* divisão*/
+
+valor /=2;
+console.log(valor);
