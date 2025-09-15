f2b51c15623bd745cec1a964a035eec037725598
giovannirp <giovanni_ge@hotmail.com>
2025-09-15 14:58:00 -0300

    feat: adicionando codigo de tempo no js

--- javaScript/niver.js
+++ javaScript/niver.js
@@ -8,9 +8,22 @@ function diasParaNiver() {
     }
     
     document.getElementById("dias").textContent = diasFaltando;
-
-
 }
+
+function atualizarHora() {
+    const horaHtml = document.getElementById("hora");
+    const agora = new Date();
+    const hora = agora.getHours();
+    const minutos = agora.getMinutes();
+    const segundos = agora.getSeconds();
+
+    horaHtml.textContent = `${hora}:${minutos}:${segundos}`;
+}
+
 
 // chamar a função
 diasParaNiver();
+
+setInterval(atualizarHora, 1000);
+
+atualizarHora();
