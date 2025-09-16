833c478da2b12073754252c36bd69d779b8cf7e1
giovannirp <giovanni_ge@hotmail.com>
2025-09-16 16:13:20 -0300

    feat: add

--- site-front/js/validacao.js
+++ site-front/js/validacao.js
@@ -12,6 +12,13 @@ form.addEventListener("submit", function(event) {
         return false;
     }
 
+    // Validação simples: tem @ e .
+    if (!email.includes("@") || !email.includes(".")) {
+        alert("Email inválido. Tente novamente");
+        return;
+    }
+
+    alert("Email cadastro com sucesso! ✅");
 
     // Limpa o campo
     form.reset(); 
