abdc082f71c919d16eda7e3e5d4bba0b34d8d30c
giovannirp <giovanni_ge@hotmail.com>
2025-09-17 16:21:48 -0300

    feat: adicionando codigo

--- site-front/js/banner.js
+++ site-front/js/banner.js
@@ -1,1 +1,19 @@
-console.log("banner")
+const imagens = ["../../img/img-1.jpg", "../../img/img-2.jpg", "../../img/img-3.jpg"];
+//                     0                  1                  2
+
+let index = 0; // começa na primeira imagem
+const banner = document.getElementById("banner");
+
+function trocarImagem() {
+    index++; // Vai para próxima imagem
+
+    if (index >= imagens.length) {
+        index = 0; // Se passou do útimo, volta para o primeiro
+    }
+
+    banner.src = imagens[index];
+}
+
+// chamar a função
+// trocarImagem();
+setInterval(trocarImagem, 3000)
