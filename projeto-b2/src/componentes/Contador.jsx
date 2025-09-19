b555e7601980d22c7bd119f890de03fc6368adde
giovannirp <giovanni_ge@hotmail.com>
2025-09-19 16:18:01 -0300

    feat: adicionando codigo

--- projeto-b2/src/componentes/Contador.jsx
+++ projeto-b2/src/componentes/Contador.jsx
@@ -1,25 +1,29 @@
-import { Fragment } from "react"
+import "./Contador.css";
+
+import { useState } from "react"
 
 const Contador = () => {
+    // Inicializa o estado com valor 0
+    const [count, setCount] = useState(0);
 
     const aumentar = () => {
-        alert("Olá Rodriguinho 1");
+        setCount(count + 1);
     }
 
     const diminuir = () => {
-        alert("Olá Rodriguinho 2");
+         setCount(count - 1);
     }
 
     return (
-        <Fragment>
-            <h1>Contador</h1>
+        <main className="container">
+            <h1>{count}</h1>
 
             <section>
                 <h2>resultado</h2>
                 <button onClick={aumentar} style={{ marginRight: '10px' }}>Aumentar</button>
                 <button onClick={diminuir}>Diminuir</button>
             </section>
-        </Fragment>
+        </main>
     )
 }
 
