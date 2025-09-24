1323efb6fcaafbc447ce1c3cdee68ead90c4b442
giovannirp <giovanni_ge@hotmail.com>
2025-09-24 15:02:40 -0300

    feat: adicionando codigo

---
+++ projeto-b2/src/componentes/Header/index.jsx
@@ -0,0 +1,13 @@
+import { Fragment } from "react";
+
+const Header = () => {
+
+    return (
+        <Fragment>
+            <h1>Meu Header</h1>
+            <h2>Novo site</h2>
+        </Fragment>
+    );
+}
+
+export default Header;
