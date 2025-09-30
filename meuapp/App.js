fe09c696a36e970ee4ec10d806b8fd83602867da
giovannirp <giovanni_ge@hotmail.com>
2025-09-30 16:21:11 -0300

    feat: add

--- meuapp/App.js
+++ meuapp/App.js
@@ -1,32 +1,12 @@
 import { StyleSheet, Text, View } from 'react-native';
+import Prime from './src/Telas/Prime';
 
 export default function App() {
   return (
     <>
-      <View style={estilos.topo}>
-        <Text style={estilos.tituloHeader}>Rodrigo Popeto</Text>
-      </View>
-
       <View>
-        <Text>Popeto</Text>
+        <Prime />
       </View>
     </>
-
   );
 }
-
-const estilos = StyleSheet.create({
-  topo: {
-    width: "100%",
-    height: 150,
-    backgroundColor: "blue",
-  },
-  tituloHeader: {
-    width: "100%",
-    color: "white",
-    textAlign: "center",
-    paddingTop: 60,
-    fontSize: 20
-  }
-
-});
--- meuapp/src/Telas/Prime.js
+++ meuapp/src/Telas/Prime.js
@@ -2,8 +2,36 @@ import { View, StyleSheet, Text } from "react-native";
 
 export default function Prime() {
   return (
-    <View>
-        <Text>B1 Dev - Popeto</Text>
-    </View>
+    <>
+      <View style={estilos.topo}>
+        <Text style={estilos.tituloHeader}>Cursos SENAI</Text>
+      </View>
+
+      <View style={estilos.cursosContent}>
+        <Text style={estilos.textTitle}> - HTML Completo</Text>
+        <Text> - Informática Básica teste</Text>
+      </View>
+    </>
   )
 }
+
+const estilos = StyleSheet.create({
+  topo: {
+    width: "100%",
+    height: 150,
+    backgroundColor: "red",
+  },
+  tituloHeader: {
+    width: "100%",
+    color: "white",
+    textAlign: "center",
+    paddingTop: 60,
+    fontSize: 20
+  },
+  cursosContent: {
+    paddingTop: 25,
+  },
+  textTitle: {
+     color: "blue",
+  }
+});
