5dc68090cf6bb73748c05ac1cda5e1ef27887b87
giovannirp <giovanni_ge@hotmail.com>
2025-10-09 14:34:26 -0300

    refatorando projeto mobile

--- meuapp/App.js
+++ meuapp/App.js
@@ -1,11 +1,12 @@
-import { StyleSheet, Text, View, ScrollView } from 'react-native';
-import Prime from './src/Telas/Prime';
+import { Text, View, ScrollView } from 'react-native';
 
 export default function App() {
   return (
     <>
       <ScrollView>
-        <Prime />
+        <Text>Site</Text>
+        <Text>Site</Text>
+        <Text>Site</Text>
       </ScrollView>
     </>
   );
