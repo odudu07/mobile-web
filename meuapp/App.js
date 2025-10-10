05119ab1a56e82e345c9ba2f9782d67e222f2d63
odudu07 <eduardo.arocha2024@gmail.com>
2025-10-10 15:23:09 -0300

    Navigator

--- meuapp/App.js
+++ meuapp/App.js
@@ -1,14 +1,11 @@
-import {  Text, ScrollView } from 'react-native';
 import Header from './src/Components/Header';
 import Card from './src/Components/Card';
 
 export default function App() {
   return (
     <>
-      <ScrollView>
         <Header/> 
         <Card />
-      </ScrollView>
     </>
 
   );
